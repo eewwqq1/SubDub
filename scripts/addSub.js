c146835e4928eff3b42babc1d803753886164c9a
Spoonzio <jpan569@gmail.com>
2020-03-29 12:49:11 -0700

    Added links and pages

---
+++ scripts/addSub.js
@@ -0,0 +1,6 @@
+function clearField(){
+    document.getElementById("nameOfSub").clearField;
+    document.getElementById("priceOfSub").clearField;
+    document.getElementById("typeOfSub").set;
+    document.getElementById("dateOfSub").clearField;
+}
